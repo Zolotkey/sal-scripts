07767acc9cc86343b9588d5e42123b5157bd434c
Shea Craig <sheagcraig@gmail.com>
2020-05-01 13:40:43 -0400

    Skip trying to execute py3 cache, and set up place to skip others.

--- sal_python_pkg/sal/utils.py
+++ sal_python_pkg/sal/utils.py
@@ -253,19 +253,22 @@ def serializer(obj):
 
 def run_scripts(dir_path, cli_args=None):
     results = []
-    for script in os.listdir(dir_path):
-        script_stat = os.stat(os.path.join(dir_path, script))
-        if not script_stat.st_mode & stat.S_IWOTH:
-            cmd = [os.path.join(dir_path, script)]
-            if cli_args:
-                cmd.append(cli_args)
-            try:
-                subprocess.call(cmd, stdin=None)
-                results.append(f"'{script}' ran successfully")
-            except (OSError, subprocess.CalledProcessError):
-                results.append(f"'{script}' had errors during execution!")
-        else:
+    skip_names = {'__pycache__'}
+    scripts = (p for p in pathlib.Path(dir_path).iterdir() if p.name not in skip_names)
+    for script in scripts:
+        if script.stat().st_mode & stat.S_IWOTH:
             results.append(f"'{script}' is not executable or has bad permissions")
+            continue
+
+        cmd = [script]
+        if cli_args:
+            cmd.append(cli_args)
+        try:
+            subprocess.call(cmd)
+            results.append(f"'{script}' ran successfully")
+        except (OSError, subprocess.CalledProcessError):
+            results.append(f"'{script}' had errors during execution!")
+
     return results
 
 
